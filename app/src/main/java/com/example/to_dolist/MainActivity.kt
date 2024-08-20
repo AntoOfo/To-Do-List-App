66e051d6d8dd3951580ec3a379d28ff858834cd7
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-08-20 23:33:08 +0100

    Basic homepage xml

--- app/src/main/java/com/example/to_dolist/MainActivity.kt
+++ app/src/main/java/com/example/to_dolist/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.to_dolist
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val title = findViewById<TextInputEditText>(R.id.titleInput)
+        val desc = findViewById<TextInputEditText>(R.id.descInput)
+        val date = findViewById<TextInputEditText>(R.id.dateInput)
+        val addBtn = findViewById<Button>(R.id.addBtn)
+        val viewBtn = findViewById<Button>(R.id.viewBtn)
+
+        addBtn.setOnClickListener{
+
+        }
+
         }
     }
-}
